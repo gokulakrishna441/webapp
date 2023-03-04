6bfdc7b9e8076be8cfd984a75bf8c8efb9c72d3a
gokul <gokul@gmail.com>
2023-03-04 15:30:05 +0530

    third project

---
+++ project3/program.js
@@ -0,0 +1,15 @@
+
+// const username=document.getElementById('username');
+// const password=document.getElementById('password');
+// const dob=document.getElementById('dob');
+// const age=document.getElementById('age');
+
+var button=document.getElementById('button');
+
+button.addEventListener('click',e =>{
+ e.preventDefault();
+ console.log("hi gokul");
+ alert("gokul")
+})
+
+
